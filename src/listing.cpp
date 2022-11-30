036516cf41641c379f1dbe0bac5ea05199443b1a
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2022-11-30 14:24:39 -0500

    working on listing

--- src/listing.cpp
+++ src/listing.cpp
@@ -2,6 +2,8 @@
 
 void cppParser::Listing::RecordListing(cppParser::Listing::ClassEntry entry) { entries[entry.interface].push_back(std::move(entry)); }
 
+void cppParser::Listing::RecordListing(cppParser::Listing::DerivedEntry entry) { derivedEntries[entry.interface].push_back(std::move(entry)); }
+
 cppParser::Listing &cppParser::Listing::Get() {
     if (listing == nullptr) {
         listing = std::shared_ptr<Listing>(new Listing());
@@ -13,6 +15,15 @@ cppParser::Listing &cppParser::Listing::Get() {
 std::ostream &cppParser::operator<<(std::ostream &os, const cppParser::Listing &listing) {
     for (const auto &interface : listing.entries) {
         os << "# " << interface.first << std::endl;
+
+        if (listing.derivedEntries.count(interface.first)) {
+            os << "## Derived Components" << std::endl;
+
+            for (const auto &classEntry : listing.derivedEntries.at(interface.first)) {
+                os << classEntry;
+            }
+        }
+
         for (const auto &classEntry : interface.second) {
             os << classEntry;
         }
@@ -41,6 +52,11 @@ std::ostream &cppParser::operator<<(std::ostream &os, const cppParser::Listing::
     return os;
 }
 
+std::ostream &cppParser::operator<<(std::ostream &os, const cppParser::Listing::DerivedEntry &classEntry) {
+    os << "- " << classEntry.className << (classEntry.defaultConstructor ? "*" : "") << std::endl;
+    return os;
+}
+
 void cppParser::Listing::ReplaceListing(std::shared_ptr<Listing> replacementListing) { listing = std::move(replacementListing); }
 
 bool cppParser::Listing::ClassEntry::operator==(const cppParser::Listing::ClassEntry &other) const {
@@ -50,3 +66,5 @@ bool cppParser::Listing::ClassEntry::operator==(const cppParser::Listing::ClassE
 bool cppParser::Listing::ArgumentEntry::operator==(const cppParser::Listing::ArgumentEntry &other) const {
     return name == other.name && interface == other.interface && description == other.description;
 }
+
+bool cppParser::Listing::DerivedEntry::operator==(const cppParser::Listing::DerivedEntry &other) const { return className == other.className && interface == other.interface; }
